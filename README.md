e193a1022bd7b5dbca62f41b75e2add940c57dd5
santimat <santinomaturo1@gmail.com>
2025-07-09 17:09:20 -0300

    Add navigation padding and enhance event handling in JavaScript

--- README.md
+++ README.md
@@ -13,3 +13,9 @@
 -   Difference btw these.
     Function Declaration log in completly.
     Space is reserve for Expression but is not asigned a function yet.
+
+### Event Bubbling
+
+-   It is when a user shoots an event, but it also shoots an event for another element.
+    That means if you click on a container element, for example, it will run the function associated with it, but it will also run the other functions associated with its child elements.
+    By default, the child element propagates the event to its parent elements.
--- events/index.js
+++ events/index.js
@@ -1,4 +1,51 @@
 // run when html document is ready
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("hi");
+// document.addEventListener("DOMContentLoaded", () => {
+//     console.log("hi");
+// });
+
+// const nav = document.querySelector(".navegacion");
+
+// // run when user leave an element
+// nav.addEventListener("mouseout", () => {
+//     nav.style.background = "transparent";
+// });
+
+// // run when user enter on the nav element
+// nav.addEventListener("mouseenter", () => {
+//     nav.style.background = "white";
+// });
+
+const search = document.querySelector(".busqueda");
+
+// run when input value's change
+search.addEventListener("input", (e) => {
+    console.log(e.target.value);
+});
+
+// copy something
+search.addEventListener("copy", (e) => {
+    e.preventDefault(); // blocked copy with ctrl+c for the users
+});
+
+// for stop Event Bubblig using e.stopPropagation()
+
+const cards = document.querySelectorAll(".hacer .card");
+const infoDivs = document.querySelectorAll(".hacer .card .info");
+const cardTitles = document.querySelectorAll(".hacer .card .titulo");
+cards.forEach((c) => {
+    c.addEventListener("click", () => {
+        console.log("im a card");
+    });
+});
+
+infoDivs.forEach((iD) => {
+    iD.addEventListener("click", () => {
+        console.log("im a div with information");
+    });
+});
+
+cardTitles.forEach((cT) => {
+    cT.addEventListener("click", () => {
+        console.log("im a title card");
+    });
 });
