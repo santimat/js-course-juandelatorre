f16264a92166f2d4a951aa901e1cc20216daa2ad
santimat <santinomaturo1@gmail.com>
2025-07-26 21:30:09 -0300

    fix: correct class name and enhance error/correct message styling in CSS; update HTML select options for better accessibility

--- quote-insurance/index.js
+++ quote-insurance/index.js
@@ -9,6 +9,52 @@ function Secure(brand, year, type) {
     this.type = type;
 }
 
+// perform the quote with the data
+Secure.prototype.performQuote = function () {
+    /* 
+        1 = American 1.15x
+        2 = Asian 1.05x
+        3 = European 1.35x
+    */
+
+    let quantity;
+    const base = 2000;
+
+    // + to convert a string to a number
+    switch (+this.brand) {
+        case 1:
+            quantity = base * 1.15;
+            break;
+        case 2:
+            quantity = base * 1.05;
+
+            break;
+        case 3:
+            quantity = base * 1.35;
+            break;
+        default:
+            break;
+    }
+
+    // read the year
+    const difference = new Date().getFullYear() - this.year;
+
+    // Every year where the difference is greatest, the cost will be reduced by 3%
+    quantity -= quantity * Math.min(difference * 0.03, 2);
+
+    /* 
+        If it's the basic secure is multiplies by 30% more 
+        else it's the complete secure is multiplies by 50% more 
+    */
+    if (this.type == "basic") {
+        quantity *= 1.3;
+    } else {
+        quantity *= 1.5;
+    }
+
+    return quantity;
+};
+
 function UI() {}
 
 // Fill the options of the years
@@ -30,6 +76,54 @@ UI.prototype.fillOptions = () => {
         selectYear.appendChild(option);
     }
 };
+
+// Show alerts on the screen
+UI.prototype.showAlerts = (msg, type) => {
+    const form = $("#quote-form"),
+        prevAlert = form.querySelector(".message");
+
+    if (prevAlert) {
+        prevAlert.remove();
+    }
+    const div = document.createElement("DIV");
+    div.classList.add(
+        "message",
+        "mt-10",
+        type == "error" ? "error" : "correct"
+    );
+    div.textContent = msg;
+
+    // Insert in form
+    // when is used insertBefore the first param is the new node and the second param is the HTML reference
+    form.insertBefore(div, $("#result"));
+
+    setTimeout(() => {
+        div.remove();
+    }, 2000);
+};
+
+UI.prototype.showResult = (secure, total) => {
+    // Create the result
+    const resultDiv = $("#result");
+    if (resultDiv.firstChild) {
+        resultDiv.firstChild.remove();
+    }
+
+    const div = document.createElement("DIV");
+    div.classList.add("mt-10");
+    div.innerHTML = `
+    <p class="header">Your resume</p> 
+    <p class="font-bold">Total: ${total}</p> 
+    `;
+    // Show spinner
+    const spinner = $("#loading");
+    spinner.style.display = "block";
+    setTimeout(() => {
+        spinner.style.display = "none";
+        resultDiv.appendChild(div);
+    }, 2000);
+};
+
 // Instatiate UI
 const ui = new UI();
 
@@ -37,12 +131,44 @@ const ui = new UI();
 document.addEventListener("DOMContentLoaded", () => {
     // Fill select with the years
     ui.fillOptions();
+
+    // Load the listeners once the DOM is loaded
+    eventListeners();
 });
 
-eventListeners();
 function eventListeners() {
     const form = $("#quote-form");
     form.addEventListener("submit", quoteInsurance);
 }
 
 // Functions
+function quoteInsurance(e) {
+    e.preventDefault();
+
+    // Read the selected brand, the year and type
+    const brand = $("#brand").value;
+
+    const year = $("#year").value;
+
+    const type = $("input[name='type']:checked").value;
+
+    if (!brand || !year || !type) {
+        ui.showAlerts("You must complete the all fields", "error");
+        return;
+    }
+
+    ui.showAlerts("Cotizando...", "success");
+
+    // Instatiate secure
+    const secure = new Secure(brand, year, type);
+
+    const total = secure.performQuote();
+
+    // Use the prototype to perform
+    ui.showResult(secure, total);
+
+    // Hide the previous quotes
+    const results = $("#result div");
+    // ? is to ask if the element already exists in HTML
+    results?.remove();
+}
